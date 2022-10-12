01d6abc774d84a78e741cbc2fd7025fd49746389
Pranjali Sanjay Shimpi <pranjalishimpi05@gmail.com>
2022-10-12 10:41:05 +0530

    Add files via upload

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  hostUrl: 'http://localhost:8080'
+  production: false
 };
 
 /*
